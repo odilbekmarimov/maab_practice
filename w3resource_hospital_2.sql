10d5c4079fc17ba9d2fb038156dafe1dc225c95a
odilbekmarimov <o.marimov@newuu.uz>
2024-10-12 14:54:21 +0500

    Update w3resource_hospital_2.sql

--- w3resource_hospital_2.sql
+++ w3resource_hospital_2.sql
@@ -49,4 +49,65 @@ where unavailable='t'
 group by blockfloor, blockcode
 order by blockfloor, blockcode
 --26
+select blockfloor AS Floor, COUNT(*) AS "No of available rooms"
+from Rooms
+where unavailable = 'f'
+group by blockfloor
+having COUNT(*) = (
+    select MAX(CountAvail)
+    from (
+        select COUNT(*) AS CountAvail
+        from Rooms
+        where unavailable = 'f'
+        group by blockfloor
+    ) as Subquery
+)
+
+--27
+
+select blockfloor AS Floor, COUNT(*) AS "No of available rooms"
+from Rooms
+where unavailable = 'f'
+group by blockfloor
+having COUNT(*) <> (
+    select MAX(CountAvail)
+    from (
+        select COUNT(*) AS CountAvail
+        from Rooms
+        where unavailable = 'f'
+        group by blockfloor
+    ) as Subquery
+)
+
+--28
+select p.name as Patient, r.roomnumber as Room, r.blockfloor as Floor, r.blockcode as block 
+from patient p join stay s 
+on p.ssn=s.patient
+inner join Room r  --rooms or room
+on s.room=r.roomnumber
+
+--29
+select n.name as Nurse, on_call.blockcode as [Block]
+from nurse n inner JOIN
+on_call on 
+n.employeeid=on_call.nurse
+order by n.name, on_call.blockcode
+
+--30
+select pa.name as Patient,
+ph.name as Physician,
+n.name as Nurse,
+s.end_time as [Date of release],
+pr.name as [treatment goint on],
+r.roomnumber as Room,
+r.blockfloor as FLOOR,
+r.blockcode as [Block]
+from patient pa
+ --- problem joining 
+
+
+--31
+
+
+
 
